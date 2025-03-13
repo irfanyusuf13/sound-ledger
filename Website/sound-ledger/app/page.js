51cf4ad9d1ff2fdca531f06147caf2c0ec6ba61b
Mario Matthews Gunawan <mariomatthewsg09@gmail.com>
2025-03-13 15:00:59 +0700

    Making template for website

---
+++ Website/sound-ledger/app/page.js
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <main>
+        <h1 className="text-4xl font-bold text-center items-center sm:text-left">
+          Sound Ledger
+        </h1>
+      </main>
+    </div>
+  );
+}
